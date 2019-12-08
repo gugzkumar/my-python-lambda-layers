0a22a8c9b6763fdb1fb5d8c596a8ee866048622a
Gagan Tunuguntla <gugzkumar@gmail.com>
2019-12-07 23:24:09 -0500

    Fixing paths for publishing dependencies

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 pip install -r ./layer-python-crypto/requirements.txt -t ./layer-python-crypto/python/lib/python3.7/site-packages/
-zip -r layer.zip ./layer-python-crypto/*
+zip -r layer.zip ./layer-python-crypto/python/*
