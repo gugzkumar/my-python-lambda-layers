d56fd097969ceaf8a96874926f2055ae0ea5577d
Gagan Tunuguntla <gugzkumar@gmail.com>
2019-12-07 20:09:31 -0500

    remove interactive from workflow

--- generate-layer-zip.sh
+++ generate-layer-zip.sh
@@ -1,3 +1,3 @@
 FULL_PATH=$(pwd)
 docker image build -t lambda-layers-python:latest ./
-docker container run --rm -it -v "$FULL_PATH/:/usr/src/app/" lambda-layers-python:latest sh build.sh
+docker container run --rm -v "$FULL_PATH/:/usr/src/app/" lambda-layers-python:latest sh build.sh
